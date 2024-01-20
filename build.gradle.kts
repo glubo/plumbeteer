d5145ef3b58c05c38ca31e2c52fa9e13dba0b7bb
Petr Sykora <petr.sykora@gmail.com>
2024-01-20 13:40:12 +0100

    feat: add Littlekt template

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.bundles.plugins)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    group = "com.game.template"
+    version = "1.0"
+}
+
+plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+    the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
+        yarnLockMismatchReport = org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport.WARNING
+    }
+}
---
+++ game/src/commonMain/kotlin/com/game/template/Game.kt
@@ -0,0 +1,46 @@
+package com.game.template
+
+import com.lehaine.littlekt.Context
+import com.lehaine.littlekt.ContextListener
+import com.lehaine.littlekt.graphics.Color
+import com.lehaine.littlekt.graphics.Fonts
+import com.lehaine.littlekt.graphics.g2d.*
+import com.lehaine.littlekt.graphics.g2d.shape.ShapeRenderer
+import com.lehaine.littlekt.graphics.gl.ClearBufferMask
+import com.lehaine.littlekt.graphics.toFloatBits
+import com.lehaine.littlekt.math.geom.degrees
+import com.lehaine.littlekt.math.geom.radians
+import com.lehaine.littlekt.util.viewport.ExtendViewport
+import kotlin.time.Duration.Companion.milliseconds
+
+
+class Game(context: Context) : ContextListener(context) {
+
+    override suspend fun Context.start() {
+        val batch = SpriteBatch(this)
+        val shapeRenderer = ShapeRenderer(batch)
+        val viewport = ExtendViewport(960, 540)
+        val camera = viewport.camera
+        var rotation = 0.radians
+        var rotationTimer = 0.milliseconds
+
+        onResize { width, height ->
+            viewport.update(width, height, context)
+        }
+
+        onRender { dt ->
+            gl.clearColor(Color.DARK_GRAY)
+            gl.clear(ClearBufferMask.COLOR_BUFFER_BIT)
+
+            batch.use(camera.viewProjection) {
+                Fonts.default.draw(it, "Hello LittleKt!", -15f, 0f)
+                shapeRenderer.filledRectangle(-25f, 50f, 100f, 50f, rotation, color = Color.RED.toFloatBits())
+            }
+            rotationTimer += dt
+            if (rotationTimer > 10.milliseconds) {
+                rotationTimer = 0.milliseconds
+                rotation += 1.degrees
+            }
+        }
+    }
+}
